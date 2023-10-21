933394978e096e03019171b4c451841694bd3a59
Jorge Iván Solano <jorgeivan@unicauca.edu.co>
2023-10-21 15:13:54 -0500

    Arreglos

--- src/app/controllers/userhack.controller.js
+++ src/app/controllers/userhack.controller.js
@@ -7,12 +7,11 @@ const getActiveUser = (req, res) => {
     const usuariosJSON = fs.readFileSync(pathMock, 'utf-8');
     const usuarios = JSON.parse(usuariosJSON);
     const usuario = usuarios.find(user => user.session === "open");
-    console.log(usuario);
-    /* if (usuario) {
+    if (usuario) {
         res.status(200).json({ mensaje: "Usuario activo encontrado", usuario });
     } else {
         res.status(401).json({ mensaje: "Usuario activo no encontrado" });
-    } */
+    }
 }
 
 module.exports = {
