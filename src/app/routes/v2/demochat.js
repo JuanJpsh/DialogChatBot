72c5a9152ac0d67ad502fbe6c252d656715c2336
Jorge Iván Solano <jorgeivan@unicauca.edu.co>
2023-10-21 15:18:03 -0500

    Cambios de rutas

--- src/app/routes/v2/demochat.js
+++ src/app/routes/v2/demochat.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const router = Router();
-const demochatController = require("../../controllers/demochat.controller");
+const demoChatController = require("../../controllers/demochat.controller");
+const bookingsController = require("../../controllers/bookings.controller")
+const authHackController = require("../../controllers/authhack.controller")
+const userHackController = require("../../controllers/userhack.controller")
 
 router
-  .post("/message", demochatController.getMessage)
+  .get("/message", demoChatController.getMessage)
+  .get("/bookings", bookingsController.getBookings)
+  .post("/bookings/create",bookingsController.createBooking)
+  .post("/login", authHackController.login)
+  .get("/active-session",userHackController.getActiveUser)
 
 module.exports = router;
