eb03233e94553945135368fbe538416f0636e834
Jorge Iván Solano <jorgeivan@unicauca.edu.co>
2023-10-21 14:58:51 -0500

    Login, userEndpoints, BookingEndpoints

--- src/app/routes/v2/demo-chat.js
+++ src/app/routes/v2/demo-chat.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const router = Router();
 const demoChatController = require("../../controllers/demo-chat.controller");
+const bookingsController = require("../../controllers/bookings.controller")
+const authHackController = require("../../controllers/authhack.controller")
+const userHackController = require("../../controllers/userhack.controller")
 
 router
   .get("/question", demoChatController.askQuestion)
+  .get("/bookings", bookingsController.getBookings)
+  .post("/bookings/create",bookingsController.createBooking)
+  .post("/login", authHackController.login)
+  .get("/active-session",userHackController.getActiveUser)
 
 module.exports = router;
